54d51d332c8721d0f346478356612d632de540c6
Lance Carlson <lcarlson@healpay.com>
2014-08-18 18:08:13 -0400

    changed req and out parameters to use stdin and stdout. Started to work on runTransaction

--- usaepay-cli.go
+++ usaepay-cli.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"fmt"
 	"flag"
 	"io/ioutil"	
 //	"encoding/xml"
@@ -9,47 +10,59 @@ import (
 	"os"
 )
 
+var Usage = func() {
+	fmt.Fprintf(os.Stderr, "Usage of %s [command]:\n", os.Args[0])
+	flag.PrintDefaults()
+}
+
 func main() {
-	location := flag.String("location", "", "usaepay endpoint")
-	action := flag.String("action", "", "API request action")
-	key := flag.String("key", "", "gateway source key")
-	pin := flag.String("pin", "", "gateway pin")
-	reqFile := flag.String("req", "", "path to request file (json)")
-	outFile := flag.String("out", "", "path to output file")
-	debug := flag.Bool("debug", false, "debug mode")
-	flag.Parse()
-	if flag.NFlag() == 0 {
-		flag.PrintDefaults()
-		os.Exit(0)
+	cmd := ""
+	if len(os.Args) > 1 {
+		cmd = os.Args[1]
 	}
 
-	// Read req file
-	reqData, err := ioutil.ReadFile(*reqFile)
-	if err != nil { log.Panic(err.Error()) }
+	flags := flag.NewFlagSet(cmd, flag.ExitOnError)
 
+	location := flags.String("location", "", "usaepay endpoint")
+	key := flags.String("key", "", "gateway source key")
+	pin := flags.String("pin", "", "gateway pin")
+	debug := flags.Bool("debug", false, "debug mode")
+
+	flags.Parse(os.Args[2:])
+
+	// Read req file
+	in, err := ioutil.ReadAll(os.Stdin)
+	if err != nil { log.Panic(err.Error()) }
 	token := usaepay.NewToken(*key, *pin)
 
 	var req usaepay.Request
 	var res usaepay.Response
 	var body []byte
-	if (*action == "getTransactionReport") {
+	switch cmd {
+	case "getTransactionReport":
 		req = new(usaepay.GetTransactionReportRequest)
 		req.SetToken(token)
-		body, err = usaepay.JSONToXML(req, reqData)
+		body, err = usaepay.JSONToXML(req, in)
 		if err != nil { log.Panic(err.Error()) }
 		res = new(usaepay.GetTransactionReportResponse)
-	} else if (*action == "searchTransactionsCustom") {
+	case "searchTransactionsCustom":
 		req = new(usaepay.SearchTransactionsCustomRequest)
 		req.SetToken(token)
-		body, err = usaepay.JSONToXML(req, reqData)
+		body, err = usaepay.JSONToXML(req, in)
 		if err != nil { log.Panic(err.Error()) }
 		res = new(usaepay.SearchTransactionsCustomResponse)
-	} else if (*action == "searchCustomers") {
+	case "searchCustomers":
 		req = new(usaepay.SearchCustomersRequest)
 		req.SetToken(token)
-		body, err = usaepay.JSONToXML(req, reqData)
+		body, err = usaepay.JSONToXML(req, in)
 		if err != nil { log.Panic(err.Error()) }
 		res = new(usaepay.SearchCustomersResponse)
+	case "runTransaction":
+		req = new(usaepay.RunTransactionRequest)
+		req.SetToken(token)
+		body, err = usaepay.JSONToXML(req, in)
+		if err != nil { log.Panic(err.Error()) }
+		res = new(usaepay.RunTransactionResponse)
 	}
 
 	if *debug { log.Println(string(body)) }
@@ -61,6 +74,5 @@ func main() {
 	b, err := res.Decode(fullBody)
 	if err != nil { log.Panic(err.Error()) }
 	// write whole the body
-	err = ioutil.WriteFile(*outFile, b, 0644)
-	if err != nil { panic(err) }
+	os.Stdout.Write(b) 
 }
